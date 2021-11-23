de06df28ed339b0596169c2144e414f96aecf68d
Swaroop <swaroopbhat710@gmail.com>
2021-11-23 17:02:50 +0530

    updated scripts

--- finalscript.sh
+++ finalscript.sh
@@ -1,12 +1,9 @@
 rm output_script.txt
 touch output_script.txt
-taskset -c 0 ./kernels/bfs -g 20 -n 1 | grep -E Generate\|Build\|Average >> output_script.txt
-taskset -c 0-1 ./kernels/bfs -g 20 -n 1 | grep -E Generate\|Build\|Average >> output_script.txt
-taskset -c 0-2 ./kernels/bfs -g 20 -n 1 | grep -E Generate\|Build\|Average >> output_script.txt
-taskset -c 0-3 ./kernels/bfs -g 20 -n 1 | grep -E Generate\|Build\|Average >> output_script.txt
-taskset -c 0-4 ./kernels/bfs -g 20 -n 1 | grep -E Generate\|Build\|Average >> output_script.txt
-taskset -c 0-5 ./kernels/bfs -g 20 -n 1 | grep -E Generate\|Build\|Average >> output_script.txt
-taskset -c 0-6 ./kernels/bfs -g 20 -n 1 | grep -E Generate\|Build\|Average >> output_script.txt
-taskset -c 0-7 ./kernels/bfs -g 20 -n 1 | grep -E Generate\|Build\|Average >> output_script.txt
-python3 plot_graph.py 
+taskset -c 2 ./kernels/bfs -g 20 -n 1 | grep -E Generate\|Build\|Average >> output_script.txt
+taskset -c 2-3 ./kernels/bfs -g 20 -n 1 | grep -E Generate\|Build\|Average >> output_script.txt
+taskset -c 2-5 ./kernels/bfs -g 20 -n 1 | grep -E Generate\|Build\|Average >> output_script.txt
+taskset -c 2-9 ./kernels/bfs -g 20 -n 1 | grep -E Generate\|Build\|Average >> output_script.txt
+taskset -c 2-17 ./kernels/bfs -g 20 -n 1 | grep -E Generate\|Build\|Average >> output_script.txt
+taskset -c 2-32 ./kernels/bfs -g 20 -n 1 | grep -E Generate\|Build\|Average >> output_script.txt
 
--- hyper_threading.sh
+++ hyper_threading.sh
@@ -1,5 +1,5 @@
 rm hyper.txt
 touch hyper.txt
-taskset -c 0,5 ./kernels/bfs -g 20 -n 1 | grep -E Generate\|Build\|Average >> hyper.txt
-taskset -c 0,1 ./kernels/bfs -g 20 -n 1 | grep -E Generate\|Build\|Average >> hyper.txt
+taskset -c 15,31 ./kernels/bfs -g 20 -n 1 | grep -E Generate\|Build\|Average >> hyper.txt
+taskset -c 29,30 ./kernels/bfs -g 20 -n 1 | grep -E Generate\|Build\|Average >> hyper.txt
 python3 hyperthread.py 
